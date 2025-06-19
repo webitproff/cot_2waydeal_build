ff6ed2e85b97b2f64352b5f16bda7329eb6d38f4
sodium carbonate <webitproff@gmail.com>
2025-06-19 18:22:47 +0300

    Update 2waydeal.rc.php

--- public_html/themes/2waydeal/2waydeal.rc.php
+++ public_html/themes/2waydeal/2waydeal.rc.php
@@ -14,26 +14,28 @@ defined('COT_CODE') or die('Wrong URL.');
 // Bootstrap is needed to use the Modal, Toast, etc. components.
 Resources::addFile('lib/bootstrap/css/bootstrap.min.css');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/jquery/jquery.min.js');
+
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/fontawesome/css/all.min.css', 'css', 200);
+
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/css/modalbox.css');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/css/style.css');
+
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/fancybox/fancybox.css');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/fancybox/fancybox.umd.js');
 
 
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/perfect-scrollbar/styles-perfect-scrollbar.css'); 
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/perfect-scrollbar/perfect-scrollbar.css');
+
 Resources::linkFileFooter(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/perfect-scrollbar/perfect-scrollbar.min.js');
 Resources::linkFileFooter(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/perfect-scrollbar/js-perfect-scrollbar.js'); 
-
-
-
 
 
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/select2/select2.min.css');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/' . Cot::$cfg['defaulttheme'] . '/assets/select2/select2.min.js');
 
-Resources::linkFileFooter('lib/bootstrap/js/bootstrap.bundle.min.js');
+// Resources::linkFileFooter('lib/bootstrap/js/bootstrap.bundle.min.js');
+
 if (Cot::$cfg['headrc_consolidate']) {
     Resources::addFile('lib/bootstrap/js/bootstrap.bundle.min.js');
 } else {
