288f77dd823f2d6b4f3d8f4dd84a4e2222863265
sodium carbonate <webitproff@gmail.com>
2025-06-27 15:55:35 +0300

    Update js-perfect-scrollbar.js

--- public_html/themes/2waydeal/assets/perfect-scrollbar/js-perfect-scrollbar.js
+++ public_html/themes/2waydeal/assets/perfect-scrollbar/js-perfect-scrollbar.js
@@ -1,14 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-  const el = document.querySelector('.ps-container-breadcrumb');
-  if (el) {
-    el.style.height = '100%'; // важно для корректной высоты скроллбара
-    new PerfectScrollbar(el, {
-      suppressScrollY: true,
-      wheelPropagation: true
-    });
-  }
-}); */
-
 document.addEventListener("DOMContentLoaded", function () {
   const el = document.querySelector('.ps-container-breadcrumb');
   if (el) {
@@ -18,10 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-
 document.addEventListener('DOMContentLoaded', function () {
-  const container = document.querySelector('.ps-container');
-  if (container && typeof PerfectScrollbar !== 'undefined') {
-    new PerfectScrollbar(container);
-  }
+    const container = document.querySelector('.ps-container');
+    if (container) {
+        container.addEventListener('wheel', function (e) {
+            e.preventDefault();
+            container.scrollTop += e.deltaY;
+        }, { passive: false });
+    }
 });
