0cfd72730ef9608d65b120c47e2d4f299630133b
sodium carbonate <webitproff@gmail.com>
2025-07-07 14:10:04 +0300

    Update market.admin.default.php

--- public_html/modules/market/inc/market.admin.default.php
+++ public_html/modules/market/inc/market.admin.default.php
@@ -204,6 +204,7 @@ $t->assign(array(
 	"SEARCH_ACTION_URL" => cot_url('admin', "m=market&c=".$c, '', true),
 	"SEARCH_SQ" => cot_inputbox('text', 'sq', $sq, 'class="schstring"'),
 	"SEARCH_STATE" => cot_radiobox($state, 'state', array(0, 1, 2), array('опубликованные', 'скрытые', 'на проверке')),
+	"SEARCH_CAT_SELECT2" => cot_market_selectcat_select2($c, 'c'),
 	"SEARCH_CAT" => cot_market_selectcat($c, 'c'),
 	"SEARCH_SORTER" => cot_selectbox($sort, "sort", array('', 'costasc', 'costdesc'), array($L['market_mostrelevant'], $L['market_costasc'], $L['market_costdesc']), false),
 	'PAGENAV_PAGES' => $pagenav['main'],
